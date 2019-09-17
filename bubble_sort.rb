08978d5746cd19a97d2ac56be37e2e5001591303
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-17 14:17:39 -0600

    complete the bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,18 @@
 def bubble_sort(values)
+	l = values.length
+  loop do
+  	swapped = false
+  	(l-1).times do |i|
+  		if values[i] > values[i + 1]
+  			values[i], values[i + 1] = values[i + 1], values[i]
+  			swapped = true
+  		end
+  	end
+  	break if not swapped
+  end
+  values
 end
+
+arr = [3, 2, 6, 2, 1, 6, 0, 16]
+p bubble_sort(arr)
+
