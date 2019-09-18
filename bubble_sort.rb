bdb331fc0e48a75a6ea66d34604631dc0f32b03e
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-18 09:35:51 -0600

    add the bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,3 +14,22 @@ def bubble_sort(values)
   end
   values
 end
+
+def bubble_sort_by(arr)
+  n = arr.length - 1
+  loop do
+    swapped = false
+    n.times do |i|
+      if yield(arr[i], arr[i + 1]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  p arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
